b85977f7f2952d5c06132a4f2fc94d8e0b7b4bbc
defndaines <defndaines@gmail.com>
2017-05-29 18:16:31 -0700

    Adds base word-count function

--- src/concordance/core.clj
+++ src/concordance/core.clj
@@ -1,5 +1,14 @@
 (ns concordance.core
+  (:require [clojure.string :as string])
   (:gen-class))
+
+(defn word-count
+  "Get the word count frequencies from a given string."
+  [line]
+  (-> line
+      .toLowerCase
+      (string/split #"[^\p{Alnum}']+")
+      frequencies))
 
 (defn -main [& args]
   (prn (format "args=%s" args)))
--- test/concordance/core_test.clj
+++ test/concordance/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [concordance.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest word-count-test
+  (testing "Without punctuation."
+    (is (= {"fish" 4 "blue" 1 "one" 1 "red" 1 "two" 1}
+           (word-count "one fish two fish red fish blue fish"))))
+  (testing "With mixed upper and lower case."
+    (is (= {"fish" 4 "blue" 1 "one" 1 "red" 1 "two" 1}
+           (word-count "One Fish Two Fish Red Fish Blue Fish"))))
+  (testing "With punctuation."
+    (is (= {"fish" 4 "blue" 1 "one" 1 "red" 1 "two" 1}
+           (word-count "One Fish, Two Fish, Red Fish, Blue Fish"))))
+  (testing "With apostrophes."
+    (is (= {"ain't" 3 "a" 1 "don't" 1 "say" 1 "so" 1 "word" 1}
+           (word-count "Ain't ain't a word, so don't say ain't.")))))
